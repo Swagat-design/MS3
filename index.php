1b1f8cae1ba0057b852f78da162f695dd5a150a4
Ejaaz Khan <dev2.itmajlis@gmail.com>
2021-04-28 00:20:15 +0530

    Added Chat Bot

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $brandQuery = mysqli_query($con,$brandSql);
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link rel="stylesheet" href="css/chat-bot-style.css"/>
 <!script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <!link rel="stylesheet" href="css/bootstrap.min.css"/>
@@ -176,6 +177,59 @@ margin-left: auto;
             Copyright &copy; Swagat 
         </div>
     </footer>
+
+    <div id="chat-bot">
+        <div class="wrapper shadow-lg">
+            <div class="title d-flex justify-content-sm-between px-3">
+                Mobile Store Bot 
+                <span style="font-size: 40px; cursor: pointer" class="hide-bot">&#x2212;</span>
+            </div>
+            <div class="form">
+                <div class="bot-inbox inbox">
+                    <div class="icon">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="msg-header">
+                        <p>Hello there, how can I help you?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="typing-field">
+                <div class="input-data">
+                    <input id="data" type="text" placeholder="Type something here.." required>
+                    <button id="send-btn">Send</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $(".hide-bot").click(function(){
+                $("#chat-bot .form").toggle('slow');
+                $("#chat-bot .typing-field").toggle('slow');
+            });
+            $("#send-btn").on("click", function(){
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+                
+                // start ajax code
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+                    data: 'text='+$value,
+                    success: function(result){
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($replay);
+                        // when chat goes down the scroll bar automatically comes to the bottom
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
+    </script>
 
     <script>var CURRENCY = '<?php echo CURRENCY; ?>';</script>
 
